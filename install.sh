b642ff72c4b85dcea10fe4f7db4cb1e0b19f0030
Peter Glotfelty 🚀 <peglotfe@microsoft.com>
2020-05-30 21:46:05 +0000

    Fixed the dotfiles

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #/bin/bash
 
+set -ex
+
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install the spaceship theme.
-ZSH_CUSTOM=$(zsh -c "echo $ZSH_CUSTOM")
+ZSH_CUSTOM=~/.oh-my-zsh
+echo $ZSH_CUSTOM
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
@@ -16,3 +19,5 @@ apt-get update && apt-get install -y ruby-full build-essential
 gem install colorls
 
 cp ./.zshrc ~/.zshrc
+mkdir -p ~/.config
+cp -r ./colorls ~/.config/colorls
